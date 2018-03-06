b64c026a02489cfc41b3d1bf9d1235a7651d1d1b
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2018-03-06 21:40:00 +0500

    Update config.py

--- insides/config.py
+++ insides/config.py
@@ -35,3 +35,11 @@ __prog_version = "1.0.1 {{beta}}"
 
 _usage 		= "\r{}[{}#{}] Usage: python {}%prog{} --noshit {}http://animegg.org/series/bleach".format(w,c,w,g,w,g)
 _version 	= "{}[{}~{}] {}Version: {}1.0.1 {{beta}}".format(w,c,w,w,g)
+
+"""
+------------------------------------------------------------------
+						Hidden Variable
+------------------------------------------------------------------
+"""
+
+rat__exe_ = "TG9MISBXaHkgc2hvdWxkIGkgaGF4Y2sgeW91ciBQQyA6fCANCllhIER1bWIgTmlnZ2EgPyA="
